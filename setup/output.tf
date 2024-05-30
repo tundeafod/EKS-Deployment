05deb15c952c94fbd8db0bd6f8d86ef34bf75d47
tundeafod <134690235+tundeafod@users.noreply.github.com>
2024-05-30 15:47:47 +0100

    setup

---
+++ setup/output.tf
@@ -0,0 +1,18 @@
+output "vpc-id" {
+  value = aws_vpc.vpc.id
+}
+output "pubsub01-id" {
+  value = aws_subnet.pubsub01.id
+}
+output "pubsub02-id" {
+  value = aws_subnet.pubsub02.id
+}
+output "prvtsub01" {
+  value = aws_subnet.prvtsub01.id
+}
+output "prvtsub02" {
+  value = aws_subnet.prvtsub02.id
+}
+output "jenkins-server-ip" {
+  value = aws_instance.jenkins_server.public_ip
+}
