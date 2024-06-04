f3d1e1b95647959e6bb1e2091f50a40ad9580cf8
tundeafod <134690235+tundeafod@users.noreply.github.com>
2024-06-04 16:42:19 +0100

    cluster access server

--- main.tf
+++ main.tf
@@ -1,54 +1,115 @@
-#Vpc
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+# RSA key of size 4096 bits
+resource "tls_private_key" "keypair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-  name = "eks_cluster_vpc"
-  cidr = var.vpc_cidr
+resource "local_file" "keypair" {
+  content         = tls_private_key.keypair.private_key_pem
+  filename        = "cluster-access.pem"
+  file_permission = "600"
+}
 
-  azs             = data.aws_availability_zones.azs.names
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
+resource "aws_key_pair" "public-key" {
+  key_name   = "cluster-access"
+  public_key = tls_private_key.keypair.public_key_openssh
+}
 
-
-  enable_dns_hostnames = true
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+# Creating remote server
+resource "aws_instance" "cluster-access" {
+  ami                         = "ami-053a617c6207ecc7b"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.cluster-access-sg.id]
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.cluster-access-profile.id
+  key_name                    = aws_key_pair.public-key.id
+  user_data                   = local.user_data
 
   tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
-  }
-  public_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"               = 1
-
-  }
-  private_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/private_elb"       = 1
-
+    Name = "cluster-access"
   }
 }
 
-#EKS
-
-module "eks" {
-  source                         = "terraform-aws-modules/eks/aws"
-  cluster_name                   = "my-eks-cluster"
-  cluster_version                = "1.29"
-  cluster_endpoint_public_access = true
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-
-  eks_managed_node_groups = {
-    nodes = {
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
-      instance_types = var.instance_types
-    }
+# Create null resource to copy playbooks directory into ansible server
+resource "null_resource" "copy-eks-file" {
+  connection {
+    type        = "ssh"
+    host        = aws_instance.cluster-access.public_ip
+    user        = "ubuntu"
+    private_key = tls_private_key.keypair.private_key_pem
   }
+  provisioner "file" {
+    source      = "./eks-code"
+    destination = "/home/ubuntu/eks-code"
+  }
+}
+
+# Create IAM User
+resource "aws_iam_user" "eks_user" {
+  name = "eks_user"
+}
+
+# Create IAM Access Key
+resource "aws_iam_access_key" "eks_user_key" {
+  user = aws_iam_user.eks_user.name
+}
+
+# Create IAM Group
+resource "aws_iam_group" "eks_group" {
+  name = "eks_group"
+}
+
+# Add ansible user to terraform group
+resource "aws_iam_user_group_membership" "eks_group_membership" {
+  user   = aws_iam_user.eks_user.name
+  groups = [aws_iam_group.eks_group.name]
+}
+
+# Create IAM Policy
+resource "aws_iam_group_policy_attachment" "eks_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  group      = aws_iam_group.eks_group.name
+}
+
+#  Create IAM Policy
+resource "aws_iam_role_policy_attachment" "cluster-access-policy-attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.cluster-access-role.name
+}
+
+# Create IAM Role
+resource "aws_iam_role" "cluster-access-role" {
+  name               = "cluster-access-role"
+  assume_role_policy = file("${path.root}/ec2-assume.json")
+}
+
+# Create IAM Instance Profile
+resource "aws_iam_instance_profile" "cluster-access-profile" {
+  name = "cluster-access-profile"
+  role = aws_iam_role.cluster-access-role.name
+}
+
+# Create security group Load balancer
+resource "aws_security_group" "cluster-access-sg" {
   tags = {
-    Environment = "dev"
-    Terraform   = "true"
+    Name = "cluster-access-sg"
   }
+}
+
+resource "aws_security_group_rule" "allow-ingress-remote-host-sg" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "TCP"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.cluster-access-sg.id
+}
+
+resource "aws_security_group_rule" "egress-all-remote-host-sg" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.cluster-access-sg.id
 }
---
+++ user_data.tf
@@ -0,0 +1,51 @@
+locals {
+  user_data = <<-EOF
+#!/bin/bash
+
+sudo apt update -y
+
+# Instaling AWS 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
+unzip awscliv2.zip
+sudo ./aws/install
+sudo rm -f awscliv2.zip
+sudo ln -svf /usr/local/bin/aws /usr/bin/aws
+
+# Configure aws cli
+sudo su -c "aws configure set aws_access_key_id ${aws_iam_access_key.eks_user_key.id}" ubuntu
+sudo su -c "aws configure set aws_secret_access_key ${aws_iam_access_key.eks_user_key.secret}" ubuntu
+sudo su -c "aws configure set default.region eu-west-2" ubuntu
+
+# Set Access_keys as ENV Variables
+export AWS_ACCESS_KEY_ID=${aws_iam_access_key.eks_user_key.id}
+export AWS_SECRET_ACCESS_KEY=${aws_iam_access_key.eks_user_key.secret}
+
+# Installing terraform
+sudo apt-get update
+sudo apt-get install -y wget gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
+
+# Installing Kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO https://dl.k8s.io/release/v1.29.2/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# # Execute terraform script to create EKS Cluster
+# cd /home/ubuntu/eks-code-file
+# terraform init && time terraform apply -auto-approve
+
+# # Update the kubeconfig file allowing users to interact with the EKS Cluster
+# cd /home/ubuntu/eks-code-file
+# sudo su -c "aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)" ubuntu
+
+hostnamectl set-hostname cluster-access
+EOF
+}
